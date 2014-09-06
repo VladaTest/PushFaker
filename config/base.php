ee9f5a272a8b7d2b0ee5921dd63917f75ce5571d
Vlada Petrović <vlada.petrovic@gmail.com>
2014-09-06 14:55:49 +0200

    Move config to folder

--- config/base.php
+++ config/base.php
@@ -1,12 +1,22 @@
 <?php
 
+$config['debug'] = false;
+
 $config['cache_dir']     = '/var/log/databox';
 $config['max_clients']   = 2;
 $config['from_date_str'] = '-30 days';
 $config['to_date_str']   = 'now';
 $config['granularity']   = 43200;
-$config['provider']      = 'dump';
-$config['operations']    = [
+
+$config['provider'] = 'dump';
+
+$config['db'] = [
+	'ip'       => '{DB_IP}',
+	'username' => '{DB_USERNAME}',
+	'password' => '{DB_PASSWORD}'
+];
+
+$config['operations'] = [
     'push'    => 40,
     'select1' => 30,
     'select2' => 20,
