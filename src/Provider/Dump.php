f5f73347d81416a52e7ea71429686e3073487211
Vlada Petrović <vlada.petrovic@gmail.com>
2014-09-09 12:43:16 +0200

    Push to request bin

--- src/Provider/Dump.php
+++ src/Provider/Dump.php
@@ -8,7 +8,16 @@ class Dump extends Provider
 {
     public function runPush($data)
     {
-        usleep(1000 * rand(500, 5000));
+        // usleep(1000 * rand(500, 5000));
+        $payload = json_encode([
+            'data' => $data['data']
+        ]);
+
+        $filename = '/tmp/' . time() . '.json';
+
+        file_put_contents($filename, $payload);
+
+        exec("curl -X POST http://requestb.in/10quws91 --data @$filename");
     }
 
     public function runSelect1($data)
